403db0733209fcc00d7fd892ad88bbdaaa8b2dbe
linxj <xiangjun.lin@outlook.com>
2019-07-08 22:33:41 +0800

    install emotion

--- public/worker.js
+++ public/worker.js
@@ -1,9 +1,8 @@
-var cacheStorageKey = 'pwa-v2'
+var cacheStorageKey = 'pwa-v4'
 var cacheList = [
   '../src/index.js',
   '../src/index.css',
-  '../src/App.js',
-  '../src/App.css',
+  '../src/App.js'
 ]
 
 self.addEventListener('install', function (e) {
@@ -11,6 +10,24 @@ self.addEventListener('install', function (e) {
     caches.open(cacheStorageKey)
       .then(function (cache) { return cache.addAll(cacheList) })
       .then(function () { return self.skipWaiting() })
+  );
+});
+
+self.addEventListener('activate', function (event) {
+  event.waitUntil(
+    // 遍历 caches 里所有缓存的 keys 值
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(
+        // 获取所有不同于当前版本名称cache下的内容
+        cacheNames.filter(function (cacheNames) {
+          return cacheNames !== cacheStorageKey
+        }).map(function (cacheNames) {
+          return caches.delete(cacheNames)
+        })
+      );
+    }).then(function () {
+      return self.clients.claim()
+    })
   );
 });
 
@@ -43,23 +60,5 @@ self.addEventListener('fetch', function (event) {
           }
         );
       })
-  );
-});
-
-self.addEventListener('activate', function (event) {
-  event.waitUntil(
-    // 遍历 caches 里所有缓存的 keys 值
-    caches.keys().then(function (cacheNames) {
-      return Promise.all(
-        // 获取所有不同于当前版本名称cache下的内容
-        cacheNames.filter(function (cacheNames) {
-          return cacheNames !== cacheStorageKey
-        }).map(function (cacheNames) {
-          return caches.delete(cacheNames)
-        })
-      );
-    }).then(function () {
-      return self.clients.claim()
-    })
   );
 });
